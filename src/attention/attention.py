daf02a76c7a49881a474fd20f5bed777e896bb54
Shaurya Shubham <shaurya.s@edgenetworks.in>
2019-02-08 18:22:13 +0530

    attention with masking

---
+++ src/attention/attention.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from constant import *
+
+class Attention(nn.Module):
+    """
+    Inputs:
+        last_hidden: (batch_size, hidden_size)
+        encoder_outputs: (batch_size, max_time, hidden_size)
+    Returns:
+        attention_weights: (batch_size, max_time)
+    """
+    def __init__(self,hidden_size, method="general"):
+        super(Attention, self).__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+        if method == 'dot':
+            pass
+        elif method == 'general':
+            self.Wa = nn.Linear(hidden_size, hidden_size, bias=False)
+            #self.Wa = nn.Linear(hidden_size, (hidden_size/4), bias=False)
+        elif method == "concat":
+            self.Wa = nn.Linear(hidden_size, hidden_size, bias=False)
+            self.va = nn.Parameter(torch.FloatTensor(batch_size, hidden_size))
+        elif method == 'bahdanau':
+            self.Wa = nn.Linear(hidden_size, hidden_size, bias=False)
+            self.Ua = nn.Linear(hidden_size, hidden_size, bias=False)
+            self.va = nn.Parameter(torch.FloatTensor(batch_size, hidden_size))
+        else:
+            raise NotImplementedError
+
+    def forward(self, last_hidden, encoder_outputs, encoder_batch_len):
+        batch_size, seq_lens, _ = encoder_outputs.size()
+
+        attention_energies = self._score(last_hidden, encoder_outputs, self.method)
+        
+        # masking
+        maxlen = encoder_outputs.size(1)
+        mask = torch.arange(maxlen)[None, :] < encoder_batch_len[:, None]
+        attention_energies[~mask] = float('-inf')
+        return F.softmax(attention_energies, -1)
+
+    def _score(self, last_hidden, encoder_outputs ,method):
+        """
+        Computes an attention score
+        :param last_hidden: (batch_size, hidden_dim)
+        :param encoder_outputs: (batch_size, max_time, hidden_dim)
+        :param method: str (`dot`, `general`, `concat`, `bahdanau`)
+        :return: a score (batch_size, max_time)
+        """
+
+        #assert encoder_outputs.size()[-1] == self.hidden_size
+
+        if method == 'dot':
+            last_hidden = last_hidden.unsqueeze(-1)
+            return encoder_outputs.bmm(last_hidden).squeeze(-1)
+
+        elif method == 'general':
+            x = self.Wa(last_hidden)
+            x = x.unsqueeze(-1)
+            print x.size(),encoder_outputs.size()
+            return encoder_outputs.bmm(x).squeeze(-1)
+
+        elif method == "concat":
+            x = last_hidden.unsqueeze(1)
+            x = F.tanh(self.Wa(torch.cat((x, encoder_outputs), 1)))
+            return x.bmm(self.va.unsqueeze(2)).squeeze(-1)
+
+        elif method == "bahdanau":
+            x = last_hidden.unsqueeze(1)
+            out = F.tanh(self.Wa(x) + self.Ua(encoder_outputs))
+            return out.bmm(self.va.unsqueeze(2)).squeeze(-1)
+
+        else:
+            raise NotImplementedError
